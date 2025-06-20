b8268f61ad382765ef83fd5ff2df8e0eac49f65f
xiokevc <xiokevc@gmail.com>
2025-06-19 20:00:24 -0400

    test

---
+++ app.js
@@ -0,0 +1,11 @@
+/*
+Exercise 1: Define an empty array
+
+1) Create an empty array and assign it to a variable called `foods`.
+
+Exercise 1 has been completed for you:
+*/
+
+const foods = [];  
+
+console.log('Exercise 1 result:', foods);
